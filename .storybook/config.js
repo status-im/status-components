8d56359b7ae46b2853cbc1d93426b8aed5319f6e
Eswara Sai <eswarasai.v@gmail.com>
2019-03-15 13:07:14 +0530

    Upgraded storybook to v5
    Removed @material-ui/core dependency

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,13 @@
-import { addDecorator, configure } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
-import withBackgrounds from "@storybook/addon-backgrounds"
+import { addParameters, configure } from '@storybook/react';
+import { create } from '@storybook/theming';
 
-addDecorator(
-  withOptions({
-    theme: {
+addParameters({
+  backgrounds: [
+    { name: 'default', value: '#eef2f5', default: true },
+    { name: 'white', value: 'white' }
+  ],
+  options: {
+    theme: create({
       mainBackground: '#112 linear-gradient(to right, #112, #333)',
       mainBorder: '1px solid rgba(255,255,255,0.1)',
       mainBorderColor: 'rgba(255,255,255,0.1)',
@@ -25,16 +28,9 @@ addDecorator(
       layoutMargin: 10,
       overlayBackground:
         'linear-gradient(to bottom right, rgba(17, 17, 34, 0.6), rgba(51, 51, 51, 0.8))',
-    }
-  })
-)
-
-addDecorator(
-  withBackgrounds([
-    { name: 'default', value: '#eef2f5', default: true },
-    { name: 'white', value: 'white' }
-  ])
-)
+    })
+  }
+})
 
 function loadStories() {
   require('../src/stories')
