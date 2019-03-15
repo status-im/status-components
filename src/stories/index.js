bf2360cb4914f4926056aaba1e4c4e500852e247
Eswara Sai <eswarasai.v@gmail.com>
2019-03-15 13:07:14 +0530

    Upgraded storybook to v5
    Removed @material-ui/core dependency

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 import 'typeface-roboto'
 
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
-import { withKnobs, text, boolean, number, object, select } from '@storybook/addon-knobs/react'
 
 import { Welcome } from '@storybook/react/demo'
-import MobileButton from '../components/MobileButton'
+// import MobileButton from '../components/MobileButton'
 
 storiesOf('Welcome', module)
   .add('To Status-Storybook', () => <StatusWelcome />)
