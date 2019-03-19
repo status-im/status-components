be0e0df212253f95aee7f3c595ef439c3ed57fc2
jeluard <julien@status.im>
2019-03-19 12:51:28 +0000

    Auto generated commit

--- precache-manifest.a74db991d88b2589cf379a5797b62f5d.js
+++ precache-manifest.a74db991d88b2589cf379a5797b62f5d.js
@@ -4,14 +4,14 @@ self.__precacheManifest = [
     "url": "static/media/roboto-latin-400italic.d8bcbe72.woff2"
   },
   {
-    "url": "main.b71546bfe7bc734ae353.bundle.js"
+    "url": "main.1a70c3c524368665cd1b.bundle.js"
   },
   {
-    "revision": "fdef5fe6e7df0f447e3a",
+    "revision": "712108b7fd74503aceab",
     "url": "vendors~main.css"
   },
   {
-    "url": "vendors~main.b71546bfe7bc734ae353.bundle.js"
+    "url": "vendors~main.1a70c3c524368665cd1b.bundle.js"
   },
   {
     "revision": "987b84570ea69ee660455b8d5e91f5f1",
@@ -26,6 +26,10 @@ self.__precacheManifest = [
     "url": "static/media/roboto-latin-100italic.6232f43d.woff2"
   },
   {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
     "revision": "55536c8e9e9a532651e3cf374f290ea3",
     "url": "static/media/roboto-latin-300.55536c8e.woff2"
   },
@@ -34,43 +38,39 @@ self.__precacheManifest = [
     "url": "static/media/roboto-latin-300.a1471d1d.woff"
   },
   {
-    "revision": "d704bb3d579b7d5e40880c75705c8a71",
-    "url": "static/media/roboto-latin-100italic.d704bb3d.woff"
-  },
-  {
     "revision": "d69924b98acd849cdeba9fbff3f88ea6",
     "url": "static/media/roboto-latin-300italic.d69924b9.woff2"
-  },
-  {
-    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
-    "url": "static/media/roboto-latin-400.5d4aeb4e.woff2"
   },
   {
     "revision": "210a7c781f5a354a0e4985656ab456d9",
     "url": "static/media/roboto-latin-300italic.210a7c78.woff"
   },
   {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
     "revision": "bafb105baeb22d965c70fe52ba6b49d9",
     "url": "static/media/roboto-latin-400.bafb105b.woff"
   },
   {
-    "url": "runtime~main.b71546bfe7bc734ae353.bundle.js"
+    "url": "runtime~main.1a70c3c524368665cd1b.bundle.js"
   },
   {
     "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
     "url": "static/media/roboto-latin-400italic.9680d5a0.woff"
   },
   {
-    "revision": "510dec37fa69fba39593e01a469ee018",
-    "url": "static/media/roboto-latin-500italic.510dec37.woff2"
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "static/media/roboto-latin-500.28546717.woff2"
   },
   {
     "revision": "de8b7431b74642e830af4d4f4b513ec9",
     "url": "static/media/roboto-latin-500.de8b7431.woff"
   },
   {
-    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
-    "url": "static/media/roboto-latin-500.28546717.woff2"
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "static/media/roboto-latin-500italic.510dec37.woff2"
   },
   {
     "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
@@ -89,12 +89,16 @@ self.__precacheManifest = [
     "url": "static/media/roboto-latin-700italic.010c1aee.woff2"
   },
   {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
     "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
     "url": "static/media/roboto-latin-900.19b7a0ad.woff2"
   },
   {
-    "revision": "846d1890aee87fde5d8ced8eba360c3a",
-    "url": "static/media/roboto-latin-700italic.846d1890.woff"
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "static/media/roboto-latin-900.8c2ade50.woff"
   },
   {
     "revision": "7b770d6c53423deb1a8e49d3c9175184",
@@ -105,11 +109,7 @@ self.__precacheManifest = [
     "url": "static/media/roboto-latin-900italic.bc833e72.woff"
   },
   {
-    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
-    "url": "static/media/roboto-latin-900.8c2ade50.woff"
-  },
-  {
-    "revision": "2361c39b1759fda5d23548d93839e339",
+    "revision": "dde02d165c520338d56783f9b11a297d",
     "url": "iframe.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a53fec6866b72fe2c7a64bdf96a7dcd5.js"
+  "precache-manifest.a74db991d88b2589cf379a5797b62f5d.js"
 );
 
 workbox.clientsClaim();
